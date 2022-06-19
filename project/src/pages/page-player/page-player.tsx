8f8dd6f429e9f5e89c5db5f2e0a3389524a47805
RedX <director099@gmail.com>
2022-06-19 11:28:39 +0500

    Удалил  моки

--- project/src/pages/page-player/page-player.tsx
+++ project/src/pages/page-player/page-player.tsx
@@ -1,13 +1,12 @@
 import {FC} from 'react';
 import {Player} from "../../components/player/player";
-import {films} from "../../mock/films";
 
 export const PagePlayer: FC = () => {
   return (
     <Player
-      title={films[0].title}
-      img={films[0].img}
-      prevVideo={films[0].prevVideo}
+      title={"asd"}
+      img={"asd"}
+      prevVideo={"asd"}
     />
   )
 };
